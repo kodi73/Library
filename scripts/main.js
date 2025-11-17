77294f8a123e5860b55615e8aaba5e043b011307
kodi73 <aditya.kumar.73@outlook.com>
2025-11-18 00:54:41 +0530

    Implemented form for NewBook

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const libraryContainer = document.getElementById("library-container");
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,3 +17,44 @@ function addBookToLibrary(title, author, pages, read) {
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("1984", "George Orwell", 328, false);
+
+function displayBooks() {
+    libraryContainer.innerHTML = "";
+
+    myLibrary.forEach(book => {
+        const card = document.createElement("div");
+        card.className = "book-card";
+        card.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Read: ${book.read ? "Yes" : "No"}</p?
+        `;
+        libraryContainer.appendChild(card);
+    })
+}
+
+document.querySelector("#newBookBtn").addEventListener("click", () => {
+    document.querySelector("#bookDialog").showModal();
+});
+
+document.querySelector("#closeDialog").addEventListener("click", () => {
+    document.querySelector("#bookDialog").close();
+})
+
+document.querySelector("#bookForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").value;
+
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+
+    displayBooks();
+    this.reset();
+    document.querySelector("#bookDialog").close();
+})
+displayBooks()
