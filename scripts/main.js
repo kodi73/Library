93bcf004715f76fc44fed1f71fe9ecafed3a33e1
kodi73 <aditya.kumar.73@outlook.com>
2025-11-17 23:21:47 +0530

    Boilerplate and Book() completed

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(t) {
+    
+}
