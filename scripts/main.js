e385bfc76cda3e0ad6436782434ca6a046c3b920
kodi73 <aditya.kumar.73@outlook.com>
2025-12-25 23:36:06 +0530

    daffadsf :wq evert "Final Project without UI"
    This reverts commit 253f37c5a67adbdd59c6ec6e67b87d63a3696963.

--- scripts/main.js
+++ scripts/main.js
@@ -10,10 +10,6 @@ function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
 }
 
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
-}
-
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
@@ -32,28 +28,10 @@ function displayBooks() {
             <h3>${book.title}</h3>
             <p>Author: ${book.author}</p>
             <p>Pages: ${book.pages}</p>
-            <p>Read: ${book.read ? "Yes" : "No"}</p>
-            <button class="toggleBtn" data-id="${book.id}">Toggle Status</button>
-            <button class="removeBtn" data-id="${book.id}">❌</button>
+            <p>Read: ${book.read ? "Yes" : "No"}</p?
         `;
         libraryContainer.appendChild(card);
     })
-}
-
-function removeBook(bookId) {
-    const index = myLibrary.findIndex(book => book.id === bookId);
-    if (index !== -1) {
-        myLibrary.splice(index, 1);
-        displayBooks();
-    }
-}
-
-function toggleReadStatus(id) {
-    const book = myLibrary.find(book => book.id === id);
-    if (book) {
-        book.toggleRead();
-        displayBooks();
-    }
 }
 
 document.querySelector("#newBookBtn").addEventListener("click", () => {
@@ -69,7 +47,7 @@ document.querySelector("#bookForm").addEventListener("submit", function(event) {
 
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
-    const pages = Number(document.querySelector("#pages").value);
+    const pages = document.querySelector("#pages").value;
     const read = document.querySelector("#read").checked;
 
     const newBook = new Book(title, author, pages, read);
@@ -79,18 +57,4 @@ document.querySelector("#bookForm").addEventListener("submit", function(event) {
     this.reset();
     document.querySelector("#bookDialog").close();
 })
-
-libraryContainer.addEventListener("click", function(event) {
-    if (event.target.classList.contains("removeBtn")) {
-        const bookId = event.target.dataset.id;
-        removeBook(bookId);
-    }
-
-    if (event.target.classList.contains("toggleBtn")) {
-        const bookId = event.target.dataset.id;
-        toggleReadStatus(bookId);
-    }
-})
-
-
 displayBooks()
